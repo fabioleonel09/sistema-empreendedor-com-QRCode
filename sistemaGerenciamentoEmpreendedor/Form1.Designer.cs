067ffe6b87d88d86847e7b238e26cee80246a01c
Fábio Leonel <fabioleonelfono@gmail.com>
2021-11-10 13:51:21 -0300

    sistema empreendedor pequenos comércios

---
+++ sistemaGerenciamentoEmpreendedor/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace sistemaGerenciamentoEmpreendedor
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbCam2 = new System.Windows.Forms.PictureBox();
+            this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.txtQRCode = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pbSemCam = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbSair = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCam2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSemCam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSair)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pbCam2);
+            this.panel1.Controls.Add(this.cboDevice);
+            this.panel1.Controls.Add(this.btnEntrar);
+            this.panel1.Controls.Add(this.txtQRCode);
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(453, 349);
+            this.panel1.TabIndex = 0;
+            // 
+            // pbCam2
+            // 
+            this.pbCam2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbCam2.Image = global::sistemaGerenciamentoEmpreendedor.Properties.Resources.noCam2;
+            this.pbCam2.Location = new System.Drawing.Point(363, 222);
+            this.pbCam2.Name = "pbCam2";
+            this.pbCam2.Size = new System.Drawing.Size(38, 31);
+            this.pbCam2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbCam2.TabIndex = 7;
+            this.pbCam2.TabStop = false;
+            // 
+            // cboDevice
+            // 
+            this.cboDevice.FormattingEnabled = true;
+            this.cboDevice.Location = new System.Drawing.Point(61, 224);
+            this.cboDevice.Name = "cboDevice";
+            this.cboDevice.Size = new System.Drawing.Size(299, 26);
+            this.cboDevice.TabIndex = 6;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar.Image = global::sistemaGerenciamentoEmpreendedor.Properties.Resources.go;
+            this.btnEntrar.Location = new System.Drawing.Point(366, 252);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(35, 35);
+            this.btnEntrar.TabIndex = 5;
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // txtQRCode
+            // 
+            this.txtQRCode.Location = new System.Drawing.Point(61, 256);
+            this.txtQRCode.Name = "txtQRCode";
+            this.txtQRCode.PasswordChar = '*';
+            this.txtQRCode.Size = new System.Drawing.Size(299, 26);
+            this.txtQRCode.TabIndex = 4;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(189, 3);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(261, 213);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label1.Location = new System.Drawing.Point(12, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 72);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sistema de\r\nGerenciamento\r\nEmpreendedor";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::sistemaGerenciamentoEmpreendedor.Properties.Resources.shop1;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 129);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.pbSemCam);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.pbSair);
+            this.panel2.Location = new System.Drawing.Point(0, 299);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(453, 50);
+            this.panel2.TabIndex = 0;
+            // 
+            // pbSemCam
+            // 
+            this.pbSemCam.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbSemCam.Image = ((System.Drawing.Image)(resources.GetObject("pbSemCam.Image")));
+            this.pbSemCam.Location = new System.Drawing.Point(316, -1);
+            this.pbSemCam.Name = "pbSemCam";
+            this.pbSemCam.Size = new System.Drawing.Size(65, 50);
+            this.pbSemCam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbSemCam.TabIndex = 2;
+            this.pbSemCam.TabStop = false;
+            this.pbSemCam.Click += new System.EventHandler(this.pbSemCam_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(3, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seguimento Brechó";
+            // 
+            // pbSair
+            // 
+            this.pbSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbSair.Image = global::sistemaGerenciamentoEmpreendedor.Properties.Resources.Close;
+            this.pbSair.Location = new System.Drawing.Point(387, -1);
+            this.pbSair.Name = "pbSair";
+            this.pbSair.Size = new System.Drawing.Size(65, 50);
+            this.pbSair.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSair.TabIndex = 0;
+            this.pbSair.TabStop = false;
+            this.pbSair.Click += new System.EventHandler(this.pbSair_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(453, 349);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmLogin";
+            this.Opacity = 0.85D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCam2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSemCam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSair)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtQRCode;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pbSair;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pbSemCam;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pbCam2;
+        private System.Windows.Forms.ComboBox cboDevice;
+    }
+}
+
---
+++ sistemaGerenciamentoEmpreendedor/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//estas bibliotecas são ativadas ao baixar e instalar seus pacotes no NuGet
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+using System.Media;
+
+namespace sistemaGerenciamentoEmpreendedor
+{
+    public partial class frmLogin : Form
+    {     
+        public frmLogin()
+        {
+            InitializeComponent();
+
+            //inabilita alguns controles
+            btnEntrar.Visible = false;
+            txtQRCode.Visible = false;
+        }
+
+        //faz a instância dos eventos
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice captureDevice;
+        SoundPlayer som;
+
+       
+        //lê a imagem no pictureBox
+        private void CaptureDevice_NewFrame(Object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+       
+
+        //Sai do sistema
+        private void pbSair_Click(object sender, EventArgs e)
+        {
+            try
+            {
+             timer1.Stop(); //para o timer
+
+             if (captureDevice.IsRunning) //caso a cam esteja funcionando
+             {
+                captureDevice.Stop(); // para a captura
+             }
+
+            }
+
+            catch
+            {
+
+            }
+
+            finally
+            {
+                Application.Exit(); //sai da aplicação
+            }      
+        }
+
+        //desativa a captura do qrcode pela webcam, ativa o txt para escrever a senha
+        private void pbSemCam_Click(object sender, EventArgs e) //implementa o botão sem cam
+        {
+            timer1.Stop(); //para o timer
+
+            try
+            {
+             if (captureDevice.IsRunning) //caso a cam esteja funcionando
+             {
+                captureDevice.Stop(); //para a captura
+             }
+
+            }
+            catch 
+            {
+
+            }
+
+            finally
+            {
+                //deixa visível os seguintes controles
+                txtQRCode.Visible = true;
+                btnEntrar.Visible = true;
+
+                //inabilita os seguintes controles
+                pictureBox.Enabled = false;
+                cboDevice.Visible = false;
+                pbCam2.Visible = false;
+                pictureBox.Visible = false;
+            }  
+        }
+
+        //ocorre quando o usuário digita a senha no txt e clica no botão para entrar
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            if (txtQRCode.Text == "admlua1030#$") //esta é a senha
+            {
+                this.Visible = false; // o frmLogin fecha
+
+                frmGerenciamento fg = new frmGerenciamento();
+                fg.Show(); // o próximo formulário abre
+
+                som = new SoundPlayer(Properties.Resources.efeitoSonoro); //instancia o som para ser executado
+                som.Play(); // o som toca
+            }
+
+            else
+            {
+                if (txtQRCode.Text == "") //caso txt vazio
+                {
+                    MessageBox.Show("Digite a senha para abrir o sistema!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Information); //exibe esta msg para o usuário
+                }
+
+                else
+                {
+                    //caso senha errada
+                    MessageBox.Show("Digite a senha correta para abrir o sistema!", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Information); //exibe essa msg para o usuário
+                    txtQRCode.Clear(); //limpa o txt do QRCode
+                }
+            }
+        }
+
+        //tudo o que inicia no evento Load do frm
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            try
+            {
+             filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice); //filtra as cams que o pc possui
+
+             if (FilterCategory.VideoInputDevice == null)
+             {
+                timer1.Stop(); //para o timer
+
+                if (captureDevice.IsRunning) //caso a cam esteja funcionando
+                {
+                    captureDevice.Stop(); //para a captura
+                }
+
+                //deixa visível os seguintes controles
+                txtQRCode.Visible = true;
+                btnEntrar.Visible = true;
+
+                //inabilita os seguintes controles
+                pictureBox.Enabled = false;
+                cboDevice.Visible = false;
+                pbCam2.Visible = false;
+                pictureBox.Visible = false;
+             }
+
+             else
+             {
+                foreach (FilterInfo filterInfo in filterInfoCollection) //sempre permance neste laço para não terminar a filtragem
+                    cboDevice.Items.Add(filterInfo.Name); //exibe as cams no comboBox
+                cboDevice.SelectedIndex = 0; //inicia comboBox com a primeira cam
+
+                txtQRCode.Clear(); // limpa o txt do qrcode
+
+                captureDevice = new VideoCaptureDevice(filterInfoCollection[cboDevice.SelectedIndex].MonikerString); //instancia a captura da cam escolhida no comboBox
+                captureDevice.NewFrame += CaptureDevice_NewFrame; //associa a cam escolhida
+                captureDevice.Start(); //inicia a cam
+                timer1.Start(); //inicia o timer
+             }
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        //mostra os eventos do timer, quando a imagem do pictureBox é reconhecida
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null) //caso o pictureBox esteja com alguma imagem capturada
+            {
+                BarcodeReader barcodeReader = new BarcodeReader(); //instancia a leitura do qrcode
+                Result result = barcodeReader.Decode((Bitmap)pictureBox.Image); //verifica se o resultado corresponde a um qrcode
+
+                if (result != null) //se o resultado for ok
+                {
+                    txtQRCode.Text += result.ToString(); // concatena a leitura do qrcode e converte para o txt ler
+
+                    if (txtQRCode.Text == "admlua1030#$") //se o qrcode for esse
+                    {
+                        som = new SoundPlayer(Properties.Resources.efeitoSonoro); //instancia o efeito sonoro
+                        som.Play(); //ativa o efeito sonoro
+
+                        timer1.Stop(); //para o timer
+                        captureDevice.Stop(); //para a captura da cam
+
+                        this.Visible = false; // o frmLogin fecha
+
+                        frmGerenciamento fg = new frmGerenciamento(); //instancia o novo frm a ser aberto
+                        fg.Show(); // o próximo formulário abre
+                    }                  
+                }
+            }
+        }
+    }
+}
